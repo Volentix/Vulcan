5fadd145c2caab13139b597dd8b5395e566b3923
realrhys <rhys@volentixlabs.com>
2019-06-26 15:20:20 +0000

    making the script into one script.

--- k3s_install.sh
+++ k3s_install.sh
@@ -8,4 +8,29 @@ chmod +x k3s
 
 mv k3s /bin
 
-sudo k3s server &
+sudo nohup  k3s server &>/dev/null &
+
+echo "Sleeping..."
+sleep 3
+
+
+curl -LO https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+
+k3s kubectl apply -f local-path-storage.yaml
+
+sudo k3s kubectl delete -f /var/lib/rancher/k3s/server/manifests/traefik.yaml
+
+cd src/helm
+
+cp istio-init.tgz /var/lib/rancher/k3s/server/static/charts/istio-init.tgz
+
+cp istio.tgz /var/lib/rancher/k3s/server/static/charts/istio.tgz
+
+
+cd ../kube/
+
+k3s kubectl apply -f 1.istio-namespace.yaml
+
+k3s kubectl apply -f 2.istio-init.yaml
+
+k3s kubectl apply -f 3.istio-main.yaml
